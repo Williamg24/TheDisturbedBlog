813155636605713f6470a69a9412a9ad21e28bde
William Guo <wguo30@stuy.edu>
2022-11-09 22:51:19 -0500

    updated readme with launch instructions and tested functions for db_user.py

--- app/db_user.py
+++ app/db_user.py
@@ -5,23 +5,25 @@ DB_FILE="user.db"
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops -- you will use cursor to trigger db events
 
-c.execute("CREATE TABLE usernames(user TEXT, pass TEXT")
-db_values = c.execute("SELECT * FROM usernames").fetchall()
+db.execute("DROP TABLE if exists usernames")
+c.execute("CREATE TABLE usernames(user TEXT, pass TEXT)")
 
 #check if username in table:
 def in_table(username):
-    user_list = list(db_values)
-    print(user_list) 
-    return username in user_list 
+    user_list = list(c.execute("SELECT user FROM usernames").fetchall())
+    print(user_list)
+    for i in user_list:
+        for j in i:
+            return username == j
+    return False
 
 #adds new username and password to table if not exist
 def add_to_db(username,password):
     if in_table(username): 
-        return "Username already exists"
+        return False
     else:
-        c.excute(f'"INSERT INTO usernames VALUES("{username}","{password}")"')
-    db.commit()
-    print(db_values) 
+        c.execute(f'INSERT INTO usernames VALUES("{username}","{password}")')
+    db.commit() 
 
 
 
@@ -31,4 +33,13 @@ def add_to_db(username,password):
 
 
 db.commit() #save changes
-db.close()  #close database
+#db.close()  #close database
+
+
+
+
+# Tests
+add_to_db("DWM","ABC") # added to table
+print(in_table("DWM")) #true
+print(in_table("MARC")) #false
+add_to_db("DWM","123") #shouldn't be added
