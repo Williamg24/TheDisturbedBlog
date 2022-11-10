929746c2d8b8bdf1ce6c28378f9160c4b5f4fbea
William Guo <wguo30@stuy.edu>
2022-11-09 23:13:48 -0500

    added and tests new methods for db_user

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-#  Scenario 2 (Web blog) by Disturbed_Window_Monsters 
+#  Our web blog by Disturbed_Window_Monsters 
 ## Roster:
 - William Guo (Project Manager)
 - Marc Jiang (Devo)
 - David Chen (Devo)
 ---
 ## Description of website/app:
-
+* TBA
 ---
 ## Launch codes:
+1) clone repo using ssh key
+    ```
+    git clone git@github.com:Williamg24 p00_Disturbed_Window_Monsters.git
+    ```
+2) run python file in app 
+    ```
+    python __init__.py
+    ```
--- app/db_user.py
+++ app/db_user.py
@@ -8,10 +8,10 @@ c = db.cursor()               #facilitate db ops -- you will use cursor to trigg
 db.execute("DROP TABLE if exists usernames")
 c.execute("CREATE TABLE usernames(user TEXT, pass TEXT)")
 
-#check if username in table:
+#check if username in table: (helper function)
 def in_table(username):
     user_list = list(c.execute("SELECT user FROM usernames").fetchall())
-    print(user_list)
+    #print(user_list)
     for i in user_list:
         for j in i:
             return username == j
@@ -25,17 +25,26 @@ def add_to_db(username,password):
         c.execute(f'INSERT INTO usernames VALUES("{username}","{password}")')
     db.commit() 
 
+#gets password of username: (helper function)
+def get_pass(username):
+    if in_table(username):
+        user_list = list(c.execute("SELECT * FROM usernames").fetchall())
+        for i in user_list:
+            if i[0] == username:
+                return i[1]
+    return False
 
-
-#command = ""          # test SQL stmt in sqlite3 shell, save as string
-#c.execute(command)    # run SQL statement
-
-
+#check if password is correct
+def correct_pass(username,password):
+    return get_pass(username) == password
 
 db.commit() #save changes
+
+
+
 #db.close()  #close database
-
-
+#command = ""          # test SQL stmt in sqlite3 shell, save as string
+#c.execute(command)    # run SQL statement
 
 
 # Tests
@@ -43,3 +52,6 @@ add_to_db("DWM","ABC") # added to table
 print(in_table("DWM")) #true
 print(in_table("MARC")) #false
 add_to_db("DWM","123") #shouldn't be added
+print(c.execute("SELECT * FROM usernames").fetchall())
+print("Password of 'DWM' is "+ get_pass("DWM")) # return ABC
+print(correct_pass("DWM","123")) # should be false
