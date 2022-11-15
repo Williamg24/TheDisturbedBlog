f357442a18116f29eda39732cee8f746139c44f3
David Chen <dchen30@stuy.edu>
2022-11-15 04:10:39 -0500

    added preview instead of entire text

--- app/__init__.py
+++ app/__init__.py
@@ -6,17 +6,20 @@
 
 from flask import Flask  # facilitate flask webserving
 from flask import render_template  # facilitate jinja templating
-from flask import request, Response, redirect, session, url_for  # facilitate form submission
-from db_user import add_to_db, correct_pass, in_table, add_post, get_posts,get_post,get_user_posts,edit_post,get_title,get_date_added,delete_post,get_unix
-import datetime, time
+# facilitate form submission
+from flask import request, Response, redirect, session, url_for
+from db_user import add_to_db, correct_pass, in_table, add_post, get_posts, get_post, get_user_posts, edit_post, get_title, get_date_added, delete_post, get_unix
+import datetime
+import time
 
 # the conventional way:
-#from flask import Flask, render_template, request
-#username = "DWM"
-#password = "ABC"
+# from flask import Flask, render_template, request
+# username = "DWM"
+# password = "ABC"
 
 app = Flask(__name__)  # create Flask object
 app.secret_key = "m4Wa0SY66R34R2fbty7P5Nmxg8fLNOQ6"
+
 
 @app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
@@ -31,13 +34,16 @@ def disp_loginpage():
     # print(request.args['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    print(get_posts())
+    # print(get_posts())
     if "username" in session:
-        return render_template('index.html', success=True, blogs = get_user_posts(session.get('username')))
+        data = get_user_posts(session.get('username'))
+        preview = " ".join(data[0][2].split()[:50]) + "..."
+        return render_template('index.html', success=True, blogs=data, preview=preview)
     else:
         return render_template('index.html', success=False)
 
-@app.route("/login", methods=['GET', 'POST'])
+
+@ app.route("/login", methods=['GET', 'POST'])
 def login():
     if request.method == "GET":
         if "username" in session:
@@ -59,7 +65,7 @@ def login():
     username = request.form['username']
     password = request.form['password']
     if in_table(username):
-        if correct_pass(username,password):
+        if correct_pass(username, password):
             session["username"] = request.form.get("username")
             return redirect("/")
         else:
@@ -67,7 +73,8 @@ def login():
     else:
         return render_template('index.html', success=False, message="Username does not exist")
 
-@app.route("/logout", methods=['POST'])
+
+@ app.route("/logout", methods=['POST'])
 def logout():
     if request.method != "POST":
         return Response(status=405)
@@ -76,7 +83,8 @@ def logout():
         session.pop("username")
     return redirect("/")
 
-@app.route("/signup", methods=['GET', 'POST'])
+
+@ app.route("/signup", methods=['GET', 'POST'])
 def signup():
     if request.method == "GET":
         return render_template('signup.html')
@@ -99,10 +107,11 @@ def signup():
     else:
         return Response(status=405)
 
-@app.route("/blog", methods=['GET', 'POST'])
+
+@ app.route("/blog", methods=['GET', 'POST'])
 def disp_blogpage():
     if request.method == "GET":
-        return render_template('blog.html', username = session.get('username'))
+        return render_template('blog.html', username=session.get('username'))
     elif request.method == "POST":
         print("\n\n\n")
         print("***DIAG: this Flask obj ***")
@@ -112,20 +121,21 @@ def disp_blogpage():
         print("***DIAG: request.args ***")
         print(request.form)
         print("***DIAG: request.args['username']  ***")
-        #print(request.form['username'])
+        # print(request.form['username'])
         print("***DIAG: request.headers ***")
         # use helper functions from db_user.py to add new blog post to database
-        if add_post(session.get("username"),request.form['title'],request.form['content'],datetime.datetime.now(),datetime.datetime.now(),0,datetime.datetime.now()):
+        if add_post(session.get("username"), request.form['title'], request.form['content'], datetime.datetime.now(), datetime.datetime.now(), 0, datetime.datetime.now()):
             return render_template('index.html', success=False, message="Failed")
         else:
             return redirect('/')
     else:
         return Response(status=405)
 
-@app.route("/view", methods=['GET', 'POST'])
+
+@ app.route("/view", methods=['GET', 'POST'])
 def view():
     if request.method == "GET":
-        return render_template('view.html', blogs = get_posts())
+        return render_template('view.html', blogs=get_posts())
     elif request.method == "POST":
         print("\n\n\n")
         print("***DIAG: this Flask obj ***")
@@ -135,45 +145,48 @@ def view():
         print("***DIAG: request.args ***")
         print(request.form)
         print("***DIAG: request.args['username']  ***")
-        #print(request.form['username'])
+        # print(request.form['username'])
         print("***DIAG: request.headers ***")
         # use helper functions from db_user.py to add new blog post to database
-        #if search_posts(request.form['search']):
+        # if search_posts(request.form['search']):
         #    return render_template('view.html', blogs = search_posts(request.form['search']))
-        #else:
+        # else:
         #    return redirect('/view')
     else:
         return Response(status=405)
 
-#dynamic routing for blog posts (blog/<slug>)
-#@app.route("/delete", methods=['GET', 'POST'])
-#def delete(username,slug):
+# dynamic routing for blog posts (blog/<slug>)
+# @app.route("/delete", methods=['GET', 'POST'])
+# def delete(username,slug):
 #    if delete_post(username,slug):
 #        return render_template('view.html')
 #    else:
 #        return Response(status=405)
 
 
-#dynamic routing for blog posts (blog/<slug>)
-@app.route("/blog/<slug>", methods=['GET', 'POST'])
+# dynamic routing for blog posts (blog/<slug>)
+@ app.route("/blog/<slug>", methods=['GET', 'POST'])
 def disp_blogpost(slug):
     if request.method == "GET":
-        return render_template('blogpost.html', username = session.get('username'), slug = slug, blog = get_post(slug))
+        return render_template('blogpost.html', username=session.get('username'), slug=slug, blog=get_post(slug))
     else:
         return Response(status=405)
 
-@app.route("/help", methods=['GET', 'POST'])
+
+@ app.route("/help", methods=['GET', 'POST'])
 def help():
     return render_template('help.html')
 
-@app.route("/back", methods=['GET', 'POST'])
+
+@ app.route("/back", methods=['GET', 'POST'])
 def back():
     return redirect('/')
 
-@app.route("/edit", methods=['GET', 'POST'])
+
+@ app.route("/edit", methods=['GET', 'POST'])
 def edit():
     if request.method == "GET":
-        return render_template('edit.html', blogs = get_user_posts(session.get('username')))
+        return render_template('edit.html', blogs=get_user_posts(session.get('username')))
     elif request.method == "POST":
         print("\n\n\n")
         print("***DIAG: this Flask obj ***")
@@ -183,32 +196,34 @@ def edit():
         print("***DIAG: request.args ***")
         print(request.form)
         print("***DIAG: request.args['username']  ***")
-        #print(request.form['username'])
+        # print(request.form['username'])
         print("***DIAG: request.headers ***")
         # use helper functions from db_user.py to add new blog post to database
     else:
         return Response(status=405)
 
-@app.route("/blog/<slug>/edit", methods=['GET', 'POST'])
+
+@ app.route("/blog/<slug>/edit", methods=['GET', 'POST'])
 def edit_blogpost(slug):
     if request.method == "GET":
-        return render_template('editblog.html', username = session.get('username'), slug = slug, blog = get_post(slug))
+        return render_template('editblog.html', username=session.get('username'), slug=slug, blog=get_post(slug))
     elif request.method == "POST":
-        if(edit_post(session.get('username'),request.form['title'],request.form['content'],get_date_added(slug),request.form['date'],get_unix(slug))):
+        if (edit_post(session.get('username'), request.form['title'], request.form['content'], get_date_added(slug), request.form['date'], get_unix(slug))):
             return redirect('/')
     else:
         return Response(status=405)
 
-@app.route("/blog/<slug>/edit/delete", methods=['GET', 'POST'])
+
+@ app.route("/blog/<slug>/edit/delete", methods=['GET', 'POST'])
 def delete(slug):
     if request.method == "GET":
-        if(delete_post(session.get('username'),get_title(slug))):
+        if (delete_post(session.get('username'), get_title(slug))):
             return redirect('/')
     else:
         return Response(status=405)
+
 
 if __name__ == "__main__":  # false if this file imported as module
     # enable debugging, auto-restarting of server when this file is modified
     app.debug = True
     app.run()
-
